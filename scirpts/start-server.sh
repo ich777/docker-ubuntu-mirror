968fffaa31536623b53014bc36e25b59c9afcab3
ich777 <christophhummer@gmail.com>
2022-11-12 06:49:34 +0100

    „scirpts/start-server.sh“ hinzufügen

---
+++ scirpts/start-server.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+if [ ! -f ${CONFIG_DIR}/postmirror.sh ]; then
+  echo "${MIRROR_DIR}/var/clean.sh" > ${CONFIG_DIR}/postmirror.sh
+  if [ ! -d ${MIRROR_DIR}/var ]; then
+    mkdir -p ${MIRROR_DIR}/var
+  fi
+  cp ${CONFIG_DIR}/postmirror.sh  ${MIRROR_DIR}/var/postmirror.sh
+  chmod +x ${MIRROR_DIR}/var/postmirror.sh
+else
+  if [ ! -d ${MIRROR_DIR}/var ]; then
+    mkdir -p ${MIRROR_DIR}/var
+  fi
+  cp ${CONFIG_DIR}/postmirror.sh  ${MIRROR_DIR}/var/postmirror.sh
+  chmod +x ${MIRROR_DIR}/var/postmirror.sh
+fi
+if [ "$(grep -E "# set base_path    /var/spool/apt-mirror" ${CONFIG_DIR}/mirror.list)" ]; then
+  sed -i "/# set base_path    \/var\/spool\/apt-mirror/c\set base_path    ${MIRROR_DIR}" ${CONFIG_DIR}/mirror.list
+  echo "---Please edit your 'mirror.list' file in your conig directory and restart the container when done!---"
+  sleep infinity
+fi
+if [ -z "$(ls -I "var" -A ${MIRROR_DIR})" ]; then
+  echo "---Starting first mirror---"
+  apt-mirror ${CONFIG_DIR}/mirror.list
+  exit 0
+fi
+if [ ! -d ${MIRROR_DIR}/mirror/$(ls ${MIRROR_DIR}/mirror/ 2>/dev/null)/ubuntu ]; then
+  echo "---Something went horribly wrong, can't find the mirror directory!---"
+  sleep infinity
+else
+  if [ ! -d /var/www/ubuntu ]; then
+    ln -s ${MIRROR_DIR}/mirror/$(ls ${MIRROR_DIR}/mirror/)/ubuntu /var/www/ubuntu
+  fi
+fi
+
+if [ "${FORCE_UPDATE}" == "true" ]; then
+  crontab -r 2>/dev/null
+  echo "---Force update enabled!---"
+  apt-mirror ${CONFIG_DIR}/mirror.list
+fi
+echo "${CRON_SCHEDULE} /usr/bin/apt-mirror ${CONFIG_DIR}/mirror.list" > ${CONFIG_DIR}/cron
+sleep 1
+crontab ${CONFIG_DIR}/cron
+echo "---'apt-mirror' will be run on the following cron schedule: ${CRON_SCHEDULE}---"
+echo "---Mirror started!---"
+echo "---Add the following line to your '/etc/apt/sources.list' file on your ubuntu installation:---"
+echo "deb http://IPFROMTECONTAINER:${APACHE2_PORT}/ubuntu stable main contrib non-free"
+echo "---Don't forget to change 'IPFROMTECONTAINER' and also change the repositories to match your config!---"
+sleep infinity
